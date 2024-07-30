67a3314aeec768328d5355a61a9009e32ec91fe9
Franco <estuardofranco5211@gmail.com>
2024-07-29 20:21:45 -0600

    Ingreso de calculadora v1

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ buttons.forEach(button => {
       button.style.setProperty("--gradient-opacity", 0);
     };
   });
+
+  //Calculadora
+
+const display= document.querySelector("#display");
+const buttonsCal= document.querySelectorAll(".calbtn");
+
+buttonsCal.forEach((btn) => {
+    btn.addEventListener("click", () =>{
+        if(btn.id === "="){
+            display.value = eval(display.value);
+        } else if (btn.id === "ac"){
+            display.value = "";
+        } else if (btn.id == "de"){
+            display.value = display.value.slice(0, -1);
+        } else {
+            display.value += btn.id
+        }
+    })
+})
